7b0fdf264f3587d0dcc733f599002722cca90c40
Celso Sá <celso.sa.mail@gmail.com>
2019-11-14 16:36:35 -0300

    Teste1

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,103 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.grey[700],
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundColor: Colors.red,
+              backgroundImage: AssetImage('images/dry.jpg'),
+            ),
+            Text(
+              'Dryelle Aguiar',
+              style: TextStyle(
+                shadows: <Shadow>[
+                  Shadow(
+                    offset: Offset(10.0, 10.0),
+                    blurRadius: 3.0,
+                    color: Colors.grey.shade800,
+                  ),
+                  Shadow(
+                    offset: Offset(10.0, 10.0),
+                    blurRadius: 8.0,
+                    color: Colors.grey.shade800,
+                  ),
+                ],
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'MULHER INCRÍVEL',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.grey.shade100,
+                fontSize: 20.0,
+                letterSpacing: 1.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.grey.shade100,
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.grey.shade700,
+                ),
+                title: Text(
+                  '+55 123 456 789',
+                  style: TextStyle(
+                    color: Colors.grey.shade800,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.grey.shade700,
+                ),
+                title: Text(
+                  'dry@email.com',
+                  style: TextStyle(
+                    color: Colors.grey.shade800,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
